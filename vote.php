ab74f04350524c9185882e600971e7de8ec17013
Longinus <jesuscaesar@outlook.com>
2021-12-20 14:01:27 +1000

    Latest update

--- vote.php
+++ vote.php
@@ -2,14 +2,10 @@
 $id = $_REQUEST['id'];
 $key = $_REQUEST['key'];
 $content = file_get_contents($id.'/rating');
-if ($content < -664) {
-    $content = -666;
-} else {
-    if ($key == 'up') {
-        $content += 1;
-    } elseif ($key == 'down') {
-        $content -= 1;
-    }
+if ($key == 'up') {
+    $content += 1;
+} elseif ($key == 'down') {
+    $content -= 1;
 }
 file_put_contents($id.'/rating', $content);
 chmod($id.'/rating', 0777);
