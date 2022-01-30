ec8940a1cbaf77d42934237b7b7134d05e97456e
Longinus <jesuscaesar@outlook.com>
2022-01-30 08:20:25 +0000

    Latest update

---
+++ manage.js
@@ -0,0 +1,43 @@
+function manage(mode, id, data) {
+    var dataString = 'mode=' + mode + '&id=' + id + '&data=' + data;
+    $.ajax({
+        type: "POST",
+        url: "manage.php",
+        data: dataString,
+        cache: false,
+        success: function(html) {
+            document.location.reload();
+        }
+    });
+    return false;
+}
+
+function interact(sub, act, obj) {
+    var dataString = 'sub=' + sub + '&act=' + act + '&obj=' + obj;
+    $.ajax({
+        type: "POST",
+        url: "interact.php",
+        data: dataString,
+        cache: false,
+        success: function(html) {
+            document.location.reload();
+        }
+    });
+    return false;
+}
+
+function launch(cmd, obj) {
+    if (cmd == 'run') {
+        window.location.href = obj;
+    } else if (cmd == 'show') {
+        window.location.href = obj + '/favicon.png';
+    } else if (cmd == 'footu') {
+        window.location.href = obj + '/footu.png';
+    } else if (cmd == 'footd') {
+        window.location.href = obj + '/footd.png';
+    } else if (cmd == 'footl') {
+        window.location.href = obj + '/footl.png';
+    } else if (cmd == 'footr') {
+        window.location.href = obj + '/footr.png';
+    }
+}
