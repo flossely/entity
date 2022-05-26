b3efe292f4978e69384a3bf70a84b34b6eba0a2b
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-26 16:19:25 +0000

    Add

--- manage.js
+++ manage.js
@@ -42,3 +42,20 @@ function batch(name, content, bulk)
     xmlhttp.open("GET","batch.php?name="+name+"&content="+content,false);
     xmlhttp.send();
 }
+function getdir(key, host = '', pkg, repo, branch = '', user, bulk)
+{
+    if (window.XMLHttpRequest) {
+        xmlhttp=new XMLHttpRequest();
+    } else {
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+        if (this.readyState==4 && this.status==200) {
+            if (bulk !== true) {
+                document.location.reload();
+            }
+        }
+    }
+    xmlhttp.open("GET","getdir.php?key="+key+"&host="+host+"&pkg="+pkg+"&repo="+repo+"&branch="+branch+"&user="+user,false);
+    xmlhttp.send();
+}
