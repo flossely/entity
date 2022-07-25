50fa28eda771328f0441f563340f739b98f98cd1
Karl August Nestor <jesuscaesar@outlook.com>
2022-07-25 06:28:22 +0000

    Latest stable version

--- civinclude.php
+++ civinclude.php
@@ -3,12 +3,6 @@
 $dir = '.';
 $list = str_replace($dir.'/','',(glob($dir.'/*.civ.php')));
 
-/*
-
-if (!is_empty($list)) {
-    foreach ($list as $key=>$value) {
-        include $value;
-    }
+foreach ($list as $key=>$value) {
+    include $value;
 }
-
-*/
