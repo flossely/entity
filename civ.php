3c9f6ba9ea01bda9368454c5a597f5bace1ae3e1
Karl August Nestor <jesuscaesar@outlook.com>
2022-07-25 06:29:32 +0000

    Latest stable version

--- civ.php
+++ civ.php
@@ -25,7 +25,7 @@ function yearconv($year)
 
 include 'civconst.php';
 include 'civilopedia.php';
-include 'civinclude.php';
+// include 'civinclude.php';
 
 $add = $_REQUEST["id"];
 $nera = $_REQUEST["era"];
