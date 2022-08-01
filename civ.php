cf8b05d01f7b0cb6049edd97803ee7c76b15ee4e
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-02 08:40:49 +1000

    Latest update

--- civ.php
+++ civ.php
@@ -1,22 +1,25 @@
 <?php
 
+// GETTING CURRENT YEAR FROM FILE
 if (file_exists('year')) {
     $today = file_get_contents('year');
 } else {
     $today = -2000;
 }
 
+// GETTING CURRENT LOCALE FROM FILE
 if (file_exists('locale')) {
     $localeOpen = file_get_contents('locale');
     $locale = ($localeOpen != '') ? $localeOpen : 'en';
 } else {
     $locale = 'en';
 }
-
 $lingua = $locale;
 
+// OPEN CIV VOCABULARY
 include 'cividictus.php';
 
+// FUNCTIONS USED IN THE SCRIPT
 function yearconv($year)
 {
     if ($year >= 0) {
@@ -28,7 +31,6 @@ function yearconv($year)
     }
     return $num . ' ' . $append;
 }
-
 function erayear($year)
 {
     if ($year < -1000) {
@@ -55,64 +57,81 @@ function erayear($year)
     return $getEra;
 }
 
+// ACCESING CONSTANTS AND ERA-DEPENDENT DATA
 include 'civconst.php';
 include 'civeramap.php';
 
+// DEFINE THE CIV ARRAY VARIABLE
 $civ = [];
 
+// GET CIV ID FROM HTTP REQUEST
 $add = $_REQUEST['id'];
+
+// GETTING ERA VALUE BY THE CURRENT YEAR
 $era = erayear($today);
 
+// DOWNLOADING SELECTED CIV DATA FILE
 if (file_exists($add.'-civ')) {
     chmod($add.'-civ', 0777);
     rename($add.'-civ', $add.'-civ.d');
 }
-
 exec('git clone https://github.com/civhub/'.$add.'-civ');
 chmod($add.'-civ', 0777);
-
 copy('./'.$add.'-civ/'.$add.'.civ.php', './'.$add.'.civ.php');
 exec('chmod -R 777 .');
 exec('rm -rf '.$add.'-civ');
-
 if (file_exists($add.'-civ.d')) {
     chmod($add.'-civ.d', 0777);
     rename($add.'-civ.d', $add.'-civ');
 }
 
-if (file_exists($add)) {
-    exec('chmod -R 777 .');
-    exec('rm -rf '.$add);
-}
-
+// INCLUDE CURRENT CIV DATA FROM DOWNLOADED FILE
 include $add.'.civ.php';
 
+// OVERRIDE CURRENT ERA VALUE IF NOT EXISTING IN CURRENT CIV DATA ARRAY
 if (!array_key_exists($era, $civ[$add]['var'])) {
     $era = array_key_first($civ[$add]['var']);
 }
 
-mkdir($add);
-chmod($add, 0777);
-file_put_contents($add.'/coord', $civ[$add]['coord']);
-chmod($add.'/coord', 0777);
-file_put_contents($add.'/rating', $civeramap[$era]['rating']);
-chmod($add.'/rating', 0777);
-file_put_contents($add.'/mode', 0);
-chmod($add.'/mode', 0777);
-file_put_contents($add.'/score', 0);
-chmod($add.'/score', 0777);
-file_put_contents($add.'/money', 0);
-chmod($add.'/money', 0777);
-file_put_contents($add.'/born', $today);
-chmod($add.'/born', 0777);
+// CREATE CIV DIRECTORY IF NOT EXISTING
+if (!file_exists($add)) {
+    mkdir($add);
+    chmod($add, 0777);
+}
 
+// SET CIV PARAMETERS IF THEY NOT EXIST TO PREVENT OVERRIDE BY NEW CIV QUERY
+if (!file_exists($add.'/coord')) {
+    file_put_contents($add.'/coord', $civ[$add]['coord']);
+    chmod($add.'/coord', 0777);
+}
+if (!file_exists($add.'/rating')) {
+    file_put_contents($add.'/rating', $civeramap[$era]['rating']);
+    chmod($add.'/rating', 0777);
+}
+if (!file_exists($add.'/mode')) {
+    file_put_contents($add.'/mode', 0);
+    chmod($add.'/mode', 0777);
+}
+if (!file_exists($add.'/score')) {
+    file_put_contents($add.'/score', 0);
+    chmod($add.'/score', 0777);
+}
+if (!file_exists($add.'/money')) {
+    file_put_contents($add.'/money', 0);
+    chmod($add.'/money', 0777);
+}
+if (!file_exists($add.'/born')) {
+    file_put_contents($add.'/born', $today);
+    chmod($add.'/born', 0777);
+}
+
+// OVERRIDING CIV DATA WITH THOSE OF THE ERA OF CURRENT YEAR
 file_put_contents($add.'/name', $civ[$add]['var'][$era]['name'][$lingua]);
 chmod($add.'/name', 0777);
 file_put_contents($add.'/leader', $civ[$add]['var'][$era]['leader'][$lingua]);
 chmod($add.'/leader', 0777);
 file_put_contents($add.'/era', $era);
 chmod($add.'/era', 0777);
-
 file_put_contents($add.'/economy', $civ[$add]['var'][$era]['economy']);
 chmod($add.'/economy', 0777);
 file_put_contents($add.'/government', $civ[$add]['var'][$era]['government']);
@@ -122,9 +141,9 @@ if (isset($civ[$add]['var'][$era]['title'])) {
     chmod($add.'/title', 0777);
 }
 
+// CREATING ERA INFO FOR SELECTED CIV
 $startyear = $civeramap[$era]['started'];
 $endyear = $civeramap[$era]['ended'];
-
 if ($startyear == INFINITY_BC) {
     $yrex = $startyear;
     $yrad = yearconv($endyear);
@@ -135,11 +154,11 @@ if ($startyear == INFINITY_BC) {
     $yrex = yearconv($startyear);
     $yrad = yearconv($endyear);
 }
-
 $erainfo = $civeramap[$era]['era'] . ' (' . $yrex . ' - ' . $yrad . ')';
 file_put_contents($add.'/erainfo.txt', $erainfo);
 chmod($add.'/erainfo.txt', 0777);
 
+// CREATING CIV INFO
 if (isset($civ[$add]['var'][$era]['title'])) {
     if ($civ[$add]['var'][$era]['government'] == DEMOCRACY || $civ[$add]['var'][$era]['government'] == FASCISM || $civ[$add]['var'][$era]['government'] == COMMUNISM) {
         $civbard = $civ[$add]['var'][$era]['title'] . ' ' . $cividictus[$lingua]['de'] . ' ' . $civ[$add]['var'][$era]['name'][$lingua] . ', ' . $civ[$add]['var'][$era]['leader'][$lingua];
@@ -149,23 +168,20 @@ if (isset($civ[$add]['var'][$era]['title'])) {
 } else {
     $civbard = $civ[$add]['var'][$era]['leader'][$lingua] . ' ' . $cividictus[$lingua]['de'] . ' ' . $civ[$add]['var'][$era]['name'][$lingua];
 }
-
 $civinfo = $civbard . ' (' . $civ[$add]['var'][$era]['economy'] . ' ' . $civ[$add]['var'][$era]['government'] . ')';
 file_put_contents($add.'/civinfo.txt', $civinfo);
 chmod($add.'/civinfo.txt', 0777);
 
+// DOWNLOADING CIV FLAG AND SETTING IT AS ICON
 if (file_exists($add.'-'.$era)) {
     chmod($add.'-'.$era, 0777);
     rename($add.'-'.$era, $add.'-'.$era.'.d');
 }
-
 exec('git clone https://github.com/civhub/'.$add.'-'.$era);
 chmod($add.'-'.$era, 0777);
-
 copy('./'.$add.'-'.$era.'/favicon.png', './'.$add.'/favicon.png');
 exec('chmod -R 777 .');
 exec('rm -rf '.$add.'-'.$era);
-
 if (file_exists($add.'-'.$era.'.d')) {
     chmod($add.'-'.$era.'.d', 0777);
     rename($add.'-'.$era.'.d', $add.'-'.$era);
