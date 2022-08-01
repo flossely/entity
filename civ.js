1cb76b01d5cf83996c54fc02ff026f040bded121
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-01 11:34:50 +1000

    Latest stable update

--- civ.js
+++ civ.js
@@ -15,17 +15,3 @@ function civ(id, bulk)
     xmlhttp.open("GET","civ.php?id="+id,false);
     xmlhttp.send();
 }
-function timeTravel(era) {
-    var eras = [];
-    eras['i'] = -2000;
-    eras['ii'] = -1000;
-    eras['iii'] = 476;
-    eras['iv'] = 1500;
-    eras['v'] = 1700;
-    eras['vi'] = 1900;
-    eras['vii'] = 1950;
-    eras['viii'] = 1990;
-    eras['ix'] = 2050;
-    eras['x'] = 2100;
-    set('year', eras[era], true);
-}
